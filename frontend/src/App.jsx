0f53e976419e3a8cf5bfaf3dfbc7518452148122
cyruu <cyruz.mhr09@gmail.com>
2024-06-16 19:18:32 +0545

    jsontoken

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register.jsx";
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <>
       <BrowserRouter>
+        <ToastContainer />
         <Routes>
           <Route path="/register" element={<Register />} />
         </Routes>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -11,6 +11,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 export default function Register() {
   const [userData, setUserData] = useState({
@@ -30,8 +31,10 @@ export default function Register() {
           },
         }
       );
+      toast.success(res.data.message);
       console.log(res);
     } catch (error) {
+      toast.error(error.message);
       console.log(error);
     }
   }
